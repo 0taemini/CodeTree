910d903a68c4eae125572ff1c2611052552756bb
0taemin <144756812+0taemini@users.noreply.github.com>
2025-02-13 17:34:24 +0900

    Title: 정수 명령 처리 6, Time: 193ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 250213/정수 명령 처리 6/process-numeric-commands-6.java
@@ -0,0 +1,52 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 0; t<T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String commend = st.nextToken();
+			
+			switch (commend) {
+			case "push":
+				pq.add(-Integer.parseInt(st.nextToken()));
+				break;
+				
+			case "pop":
+				System.out.println(-pq.poll()); 
+				break;
+				
+			case "size":
+				System.out.println(pq.size());
+				break;
+			case "empty":
+				if(pq.isEmpty()) {
+					System.out.println(1);
+				} else {
+					System.out.println(0);
+				}break;
+				
+			case "top":
+				System.out.println(-pq.peek());
+				break;
+			default:
+				break;
+			}
+		}
+		
+		
+		
+	}
+	
+	
+
+}
