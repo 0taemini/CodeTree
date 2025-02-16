200572d169e788d27c326dba50a3f321f3c4201f
0taemin <144756812+0taemini@users.noreply.github.com>
2025-02-16 16:22:14 +0900

    Title: 블럭쌓는 명령2, Time: 90ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 250216/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,28 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] map = new int[n];
+
+        for(int i = 0; i<k; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            for(int j = a; j<=b; j++){
+                map[j] +=1;
+            }
+        }
+
+
+        System.out.println(Arrays.stream(map).max().getAsInt());
+
+    }
+}
